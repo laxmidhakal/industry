d3bf5682417c1e9ce150666f566405b704056387
laxmi dhakal <laxmidhakal159@gmail.com>
2020-02-12 17:05:32 +0545

    added

--- app/Http/Controllers/IndustryController.php
+++ app/Http/Controllers/IndustryController.php
@@ -19,8 +19,8 @@ class IndustryController extends Controller
 	public function index()
 	{
 		$index_details = Slider::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
-		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
-		$about_details = About::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->latest('created_at')->first();
+		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->take(1);
+		$about_details = About::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get()->take(1);
 		$page_title = "Welcome";
 		return view('welcome',compact(['index_details','page_title','settings','about_details']));
 	}
@@ -28,7 +28,8 @@ class IndustryController extends Controller
 	public function indexAbout()
 	{
 		$about_details = About::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
-		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
+		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->take(1);
+		$about_details = About::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get()->take(1);
 		$page_title = "About";
 		return view('about',compact(['about_details','page_title','settings']));
 	}
@@ -36,7 +37,8 @@ class IndustryController extends Controller
 	public function indexCompanies()
 	{
 		$companies_details = Company::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
-		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
+		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->take(1);
+		$about_details = About::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get()->take(1);
 		$page_title = "Companies";
 		return view('companies',compact(['companies_details' ,'page_title','settings']));
 	}
@@ -44,14 +46,16 @@ class IndustryController extends Controller
 	public function indexGallery()
 	{
 		$gallery_details = Gallery::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
-		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
+		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->take(1);
+		$about_details = About::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get()->take(1);
 		$page_title = "Gallery";
 		return view('gallery' ,compact(['gallery_details' ,'page_title','settings']));
 	}
 
 	public function indexProduct()
 	{
-		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
+		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->take(1);
+		$about_details = About::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get()->take(1);
 
 		return view('product',compact('settings'));
 	}
@@ -59,14 +63,16 @@ class IndustryController extends Controller
 	public function indexTeam()
 	{
 		$team_details = Team::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
-		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
+		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->take(1);
+		$about_details = About::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get()->take(1);
 		$page_title ='Team';
 		return view('team' ,compact(['team_details' ,'page_title','settings']));
 	}
 
 	public function indexContact()
 	{
-		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get();
+		$settings=Setting::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->take(1);
+		$about_details = About::where('is_active', true)->orderBy('sort_id','DESC')->orderBy('created_at','DESC')->get()->take(1);
 		return view('contact',compact('settings'));
 	}
 }
