36a1372a33068ac84b40733fdab557a58caa9a93
laxmi dhakal <laxmidhakal159@gmail.com>
2020-02-18 11:35:53 +0545

    contact done

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Input;
 use App\Helper\Helper;
+use Illuminate\Support\Str;
 use Validator;
 use Redirect;
 use Response;
