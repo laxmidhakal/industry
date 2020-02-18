c773e4c321e019e1e2e722a40ed881b23047036b
laxmi dhakal <laxmidhakal159@gmail.com>
2020-02-18 17:03:48 +0545

    toast

--- resources/views/product.blade.php
+++ resources/views/product.blade.php
@@ -11,24 +11,24 @@
   </div>
 </section>
 <section class="features-section spad set-bg" data-setbg="{{URL::to('/')}}img/features-bg.jpg" style="background-image: url(&quot;img/features-bg.jpg&quot;);">
-    <div class="container">
-      <div class="row">
-        @foreach($product_details as $main_data)
-        <div class="col-lg-4 col-md-6">
-          <div class="feature-box">
-            <img src="{{URL::to('/')}}/images/productdetail/{{$main_data->image_enc}}" alt="{{$main_data->title}}" class="img-fluid w-100 product-img">
-            <div class="fb-text">
-              <h5>{{$main_data->title}}</h5>
-              <p>{!!$main_data->description!!} </p>
-              <a href="{{URL::to('/')}}/product/{{$main_data->getProduct->slug}}/{{$main_data->slug}}" class="fb-more-btn">Read More</a>
-            </div>
+  <div class="container">
+    <div class="row">
+      @foreach($product_details as $main_data)
+      <div class="col-lg-4 col-md-6">
+        <div class="feature-box">
+          <img src="{{URL::to('/')}}/images/productdetail/{{$main_data->image_enc}}" alt="{{$main_data->title}}" class="img-fluid w-100 product-img">
+          <div class="fb-text">
+            <h5>{{$main_data->title}}</h5>
+            <p>{!!$main_data->description!!} </p>
+            <a href="{{URL::to('/')}}/product/{{$main_data->getProduct->slug}}/{{$main_data->slug}}" class="fb-more-btn">Read More</a>
           </div>
         </div>
-        @endforeach
-        <div class="mt-4">
-          {!! $product_details->links("pagination::bootstrap-4") !!}
-        </div>
       </div>
+      @endforeach
     </div>
+    <div class="mt-4">
+      {!! $product_details->links("pagination::bootstrap-4") !!}
+    </div>
+  </div>
   </section>
 @endsection
